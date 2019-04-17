0a83b3dfee7b2349dd9a0134c4ccc6570abcc111
Austin Miller <austinmiller@satx.rr.com>
2019-04-17 10:51:25 -0500

    working on first get request

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,14 @@ var mysql = require('mysql');
 const app = express();
 const port = 3000;
 
+app.get('/home/:login/password/:password', (req, res) => {
+	con.query('SELECT user_id FROM user_info WHERE user_id =' + req.params[login] + ' AND _password =' + req.params[password] + "'" , function (error, results, fields) {
+    if (error)
+		throw error;
+	res.send(results);
+    });
+});
+
 var con = mysql.createConnection({
     host: "127.0.0.1",
     user: "dev",
