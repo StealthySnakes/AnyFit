4bc8bca7d8a09018ebcff164cf7e595f2a3d70cb
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-04-17 11:08:00 -0500

    added signup

---
+++ Frontend/src/SignupPage/Addimg.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './Addimg.css';
+
+export const Addimg = props =>(
+    <>
+        <h1 class="imghead">Upload Avatar:</h1>
+        <img src="https://placehold.it/150x150" width="150" height="150"></img>
+        <div className="form-group">
+            <label htmlFor="img"></label>
+            <input type="text" name="Img" id="addimg" placeholder="Add image link here"></input>
+            <form action="/action_page.php">
+                <input type="file" name="pic" accept="image/*"/>
+            </form>
+        </div>
+    </>
+);
+
+export default Addimg;
