a8828c847c467a3fcf8171ecb18b71fb0c1c6f80
Austin Miller <austinmiller@satx.rr.com>
2019-04-17 11:29:33 -0500

    trying to pull for updated files

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/home/:login/password/:password', (req, res) => {
-	con.query('SELECT user_id FROM user_info WHERE user_id =' + req.params[login] + ' AND _password =' + req.params[password] + "'" , function (error, results, fields) {
+	con.query('SELECT user_id FROM user_info WHERE user_id =' + req.params["login"] + ' AND _password =' + req.params["password"] + "'" , function (error, results, fields) {
     if (error)
 		throw error;
 	res.send(results);
