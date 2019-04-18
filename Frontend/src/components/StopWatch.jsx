7917134d87df21408064da47fdedcf0e9a2809d2
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-04-17 23:22:47 -0500

    manually added files to avi branch
    should still work, no new dependencies or modules added. Simply new jsx, js and css files

---
+++ Frontend/src/components/StopWatch.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+ 
+export const Stopwatch = (begin) => (
+  <>
+  <h1>00:00</h1>
+  </>
+);
+ 
+export default Stopwatch;
---
+++ Frontend/src/models/Workout.js
@@ -0,0 +1,14 @@
+export class Workout {
+    constructor(name, focus, expertise, length, intensity, exercises, rating, comments) {
+      this.name = name;
+      this.focus = focus;
+      this. expertise = expertise;
+      this.length = length;
+      this.intensity = intensity;
+      this.exercises = exercises;
+      this.rating = rating;
+      this.comments = comments;
+    }
+  }
+
+export default Workout;
