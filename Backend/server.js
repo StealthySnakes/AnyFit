ce9bc7b3c11b59903f3116018e1e95b94ff66b1a
elilaird <elaird03@outlook.com>
2019-04-24 19:33:50 +0000

    Added username validation at sign up

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ app.post('/home/:login/password/:password', (req, res) => {
 			res.send(results);
 		}
 		else {
-			res.send("null")
+			res.send("null");
 		}
 
 	console.log("Incoming login request...");
@@ -30,7 +30,7 @@ app.get('/home/:userID', (req,res) => {
 		if(results.length >= 1){
 			res.send(results);
 		}
-		console.log("Incoming request for friends data...")
+		console.log("Incoming request for friends data...");
 	});
 });
 
@@ -44,9 +44,9 @@ app.get('/home/:userID/avatar', (req,res) =>{
 			res.send(results);
 		}
 		else {
-			res.send('https://via.placeholder.com/150')
+			res.send('https://via.placeholder.com/150');
 		}
-		console.log("Incoming request for user avatar...")
+		console.log("Incoming request for user avatar...");
 	});
 });
 
@@ -62,12 +62,12 @@ app.get('/home/:userID/bio', (req,res) =>{
 		else {
 			res.send("");
 		}
-		console.log("Incoming request for user bio...")
+		console.log("Incoming request for user bio...");
 	});
 });
 
 //Return all exercises
-app.get('/exercises', (req, res) => {
+app.get('/exercises/', (req, res) => {
 con.query('SELECT exercise_name FROM exercise;' , function (error, results, fields) {
 	if (error)
 	throw error;
@@ -76,13 +76,58 @@ console.log("Incoming request for exercise name... ");
 	});
 });
 
-//Return exercise image url
-app.get('/exerciseName/:exerciseName', (req, res) => {
-con.query('SELECT exercise_image FROM exercise WHERE exercise_name = \'' + req.params['exerciseName'] + "\';" , function (error, results, fields) {
+//Return exercise image url!
+app.get('/exerciseName/:workout_id', (req, res) => {
+con.query('SELECT exercise_image FROM exercise WHERE workout_id = '+ req.params['workout_id'] + ';' , function (error, results, fields) {
 	if (error)
 	throw error;
 res.send(results);
 console.log("Incoming request for exercise image...");
+	});
+});
+
+app.get('/exercises/workout_info', (req, res) => {
+con.query('SELECT * FROM workout_info;' , function (error, results, fields) {
+	if (error)
+	throw error;
+res.send(results);
+console.log("Incoming request for workout_info... ");
+	});
+});
+
+app.put('/exercises/:workout_id/set_count/:set_count', (req, res) => {
+con.query('UPDATE workout_info SET set_count = '+ req.params['set_count'] +' WHERE workout_id = '+ req.params['workout_id'] + ';' , function (error, results, fields) {
+	if (error)
+	throw error;
+res.send(results);
+console.log("Incoming request to update workout_id's set count...");
+	});
+});
+
+app.put('/exercises/:workout_id/rep_count/:rep_count', (req, res) => {
+con.query('UPDATE workout_info SET rep_count = '+ req.params['rep_count'] +' WHERE workout_id = '+ req.params['workout_id'] + ';' , function (error, results, fields) {
+	if (error)
+	throw error;
+res.send(results);
+console.log("Incoming request to update workout_id's rep count...");
+	});
+});
+
+app.put('/exercises/:workout_id/workout_length/:workout_length', (req, res) => {
+con.query('UPDATE workout_info SET workout_length = '+ req.params['workout_length'] +' WHERE workout_id = '+ req.params['workout_id'] + ';' , function (error, results, fields) {
+	if (error)
+	throw error;
+res.send(results);
+console.log("Incoming request to update workout_id's workout length...");
+	});
+});
+
+app.put('/exercises/:workout_id/workout_desc/:workout_desc', (req, res) => {
+con.query('UPDATE workout_info SET workout_desc = '+ req.params['workout_desc'] +' WHERE workout_id = '+ req.params['workout_id'] + ';' , function (error, results, fields) {
+	if (error)
+	throw error;
+res.send(results);
+console.log("Incoming request to update workout_id's workout desc...");
 	});
 });
 
@@ -95,6 +140,24 @@ app.get('/WorkoutPage', (req, res) => {
   res.send('<h1> YOU READY TO WORKOUT BROTHER? </h1>');
   console.log('Workout page has been accessed');
 });
+
+app.get('/home/username/:username', (req,res) => {
+	con.query('SELECT COUNT(1) as user FROM user_info WHERE username = \'' + req.params['username'] + '\';', function(error,results,fields) {
+		if(error)
+			throw error;
+		if(results[0].user == 1){
+			res.send('Username already taken');
+		}
+		else if(results[0].user == 0){
+			res.send('');
+		}
+		else{
+			res.send('error');
+		}
+	});
+	console.log('Incoming username validation...');
+});
+
 var con = mysql.createConnection({
     host: "127.0.0.1",
     user: "dev",
