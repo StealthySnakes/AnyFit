08cfef5190690d01cf11143680a3c85589846114
Eli Laird <elaird03@outlook.com>
2019-04-30 22:38:46 -0500

    edited create workout

--- Backend/server.js
+++ Backend/server.js
@@ -426,7 +426,7 @@ app.post('/newWorkoutId/:workoutObject' , (req, res) => {
 	var obj = JSON.parse(req.params['workoutObject']);
 	//Insert new workout object into user_workout
 	try {
- 			con.query('INSERT INTO user_workout (user_id, workout_length, workout_desc, workout_name,intensity, ExpLevel, Time_stamp) VALUES (' +  obj['userID'] + ',' + obj['workoutDuration'] + ', \'' + obj['workoutDesc'] + '\' , \'' + obj['workoutName'] + '\',' + obj['intensity'] + ', \'' + obj['experience'] + '\', CURRENT_TIMESTAMP);', function (error, results, fields) {
+ 		con.query('INSERT INTO user_workout (user_id, workout_length, workout_desc, workout_name,intensity, ExpLevel, Time_stamp) VALUES (' +  obj['userID'] + ',' + obj['workoutDuration'] + ', \'' + obj['workoutDesc'] + '\' , \'' + obj['workoutName'] + '\',' + obj['intensity'] + ', \'' + obj['experience'] + '\', CURRENT_TIMESTAMP);', function (error, results, fields) {
     	if(error)
     		throw error;
   		});
@@ -445,6 +445,8 @@ app.post('/newWorkoutId/:workoutObject' , (req, res) => {
 	catch(err){
 		console.log(err);
 	}
+
+
 });
 
 app.get('/home/username/:username', (req,res) => {
