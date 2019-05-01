79dd1171f3bfdd646f3096141b2d01821cac9773
Eli Laird <elaird03@outlook.com>
2019-05-01 18:41:02 -0500

    added ability to pass post through body

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cors = require('cors');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 app.use(cors());
 
 // Returns a user_id
