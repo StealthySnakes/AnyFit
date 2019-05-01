4d914238821d61f70b6d41843cc0f27455e88124
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-04-30 20:17:48 -0500

    new stopwatch

--- Frontend/src/components/StopWatch.jsx
+++ Frontend/src/components/StopWatch.jsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
+import ReactStopwatch from 'react-stopwatch';
  
-export const StopWatch = (begin) => (
-  <>
-  <h1>00:00</h1>
-  </>
-);
+export class Stopwatch extends React.Component{
+
+  render(){
+    return(
+      <ReactStopwatch
+      seconds={0}
+      minutes={0}
+      hours={0}
+  
+      render={({ formatted, hours, minutes, seconds }) => {
+        if(this.props.beginClk){
+          return(
+            <h1>
+              {formatted}
+            </h1>
+          );
+        }
+        return (
+            <h1>
+              00:00:00
+            </h1>
+        );
+      }}
+     />
+    );
+  }
+}
  
-export default StopWatch;
+export default Stopwatch;
