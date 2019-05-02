9ed3af747bab1325e66c3eeb552fa32f46c52c4f
elilaird <elaird03@outlook.com>
2019-05-02 16:10:10 +0000

    fixed issues

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const bcrypt = require('bcrypt');
 app.use(cors());
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     //  to support URL-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies  
   extended: true
 })); 
 
