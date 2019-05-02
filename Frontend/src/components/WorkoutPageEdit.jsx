d144af0083778c50981c9569cec99dea2f2cfd84
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-05-01 23:41:48 -0500

    still need add exercise capability

--- Frontend/src/components/WorkoutPageEdit.jsx
+++ Frontend/src/components/WorkoutPageEdit.jsx
@@ -6,15 +6,20 @@ import { Container, Media, Row, Col, Form, Modal, Button} from 'react-bootstrap'
 import {ExerciseCard, ExerciseList} from './ExerciseCard';
 import './WorkoutPage.css';
 import {Rating} from './Rating';
+import { WorkoutRepository } from '../api/WorkoutRepository';
 
 
 export class WorkoutPageEdit extends React.Component{
+  
   constructor(){
     super();
     this.handleCustomAdditionSubmit = this.handleCustomAdditionSubmit.bind(this);
     this.removeExercise = this.removeExercise.bind(this);
+    this.newName = this.newName.bind(this);
+    this.newDesc = this.newDesc.bind(this);
+    
   }
-
+  workoutRepository = new WorkoutRepository;
     state = {
         wrkt: new Workout(
             "jimbo's stretch routine",
@@ -30,19 +35,16 @@ export class WorkoutPageEdit extends React.Component{
                 new Exercise("Planks", "lie there on the floor", "https://static-s.aa-cdn.net/img/ios/1132834831/eb7c52c5f7fd82798ff99ad6264c8727?v=1", 4,10,10),
               ],
             5,
-            [
-                new Comment("Jimbo", "my favorite workout"),
-                new Comment("Jumbo", "I am not Jimbo")
-            ]
+            'This is fun'
         ),
-        name:'placeholder',
-        description:'placeholder',
-        focus:'placeholder',
-        expertise:'placeholder',
-        length:0,
-        intensity:'placeholder',
+        name:' ',
+        description:' ',
+        focus:' ',
+        expertise:' ',
+        length:' ',
+        intensity:' ',
         exercises:[],
-        rating:0,
+        rating:' ',
         comments: [],
 
         showAddExercise: false,
@@ -50,7 +52,9 @@ export class WorkoutPageEdit extends React.Component{
         customExerciseDesciption:'placeholder',
         customExerciseDuration:'placeholder',
         customExerciseSets:1,
-        customExerciseReps:10
+        customExerciseReps:10,
+
+        disp:'display:"block"'
     }
 
 
@@ -60,6 +64,7 @@ export class WorkoutPageEdit extends React.Component{
             wrkt: new Workout(this.state.name, this.state.description, this.state.focus, this.state.expertise, this.state.length, this.state.intensity,
                 this.state.exercises, this.state.rating, this.state.comments)
         })
+        this.workoutRepository.updateWorkout(101, this.state.wrkt);
     }
 
 
@@ -76,21 +81,46 @@ export class WorkoutPageEdit extends React.Component{
                  })
     }
 
+    newName = event => {
+      this.workoutRepository.updateName(102, this.state.name);
+    }
+
+    newDesc = event =>{
+      this.workoutRepository.updateDescription(102, this.state.description);
+    }
+
+    newFocus = event =>{
+      this.workoutRepository.updateFocus(102, this.state.focus);
+    }
+
+    newLength = event =>{
+      this.workoutRepository.updateLength(102, this.state.length);
+    }
+
+    newExpertise = event =>{
+      this.workoutRepository.updateExpertise(102, this.state.expertise);
+    }
+
+    newIntensity = event =>{
+      this.workoutRepository.updateIntensity(102, this.state.intensity);
+    }
+
     removeExercise(ind){
       this.setState(
         state =>{
-          state.wrkt.exercises.splice(ind)
+          state.wrkt.exercises.splice(ind, 1)
         }
-      )
+      );
+      this.setState({disp:'display:"none"'})
+
     }
 
     render(){
         return(
-        <>
+        <body style={{}}>
             <Navigation/>
-            <div className = "card" style={{float:'right', margin:'2em', padding:'3em', position:'fixed', zIndex:1, right:'1em'}}>
-                <button className = "btn btn-primary btn-lg" onClick = {e => this.updateWrkt()}><h1>Save Changes</h1></button>
-                <button className = "btn btn-secondary btn-lg" style={{marginTop:'1em'}}><h1>Cancel</h1></button>
+            <div className = "card" style={{float:'right', margin:'0em', padding:'2em', position:'fixed', zIndex:1, right:'1em', bottom:'1em'}}>
+                <button className = "btn btn-success" style={{marginTop:'1em'}}><h3>Done Editing</h3></button>
             </div>
             <Container style={{margin:'1em'}}>               {/* Outer Container */}
               <Row >
@@ -102,18 +132,23 @@ export class WorkoutPageEdit extends React.Component{
                     <Container>
                         <Row style={{display:'block'}}>
                             <label for="name" style = {{float:'left'}}>Workout Name:</label>
-                            <input type = "text" className = "form-control" value={this.state.name} onChange={e => this.setState({name: e.target.value})}></input>
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.name} onChange={e => this.setState({name: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block" style={{marginBottom:'1em'}}onClick={this.newName}>Save</button>
                             <label for="description" style = {{float:'left'}}>Description:</label>
-                            <input type = "text" className = "form-control" value={this.state.description} onChange={e => this.setState({description: e.target.value})}></input>
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.description} onChange={e => this.setState({description: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block"onClick={this.newDesc}>Save</button>
                             <label for="focus" style={{float:'left'}}>Focus:</label>
-                            <input type = "text" className = "form-control" value={this.state.focus} onChange={e => this.setState({focus: e.target.value})}></input>
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.focus} onChange={e => this.setState({focus: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block"style={{marginBottom:'1em'}}onClick={this.newFocus}>Save</button>
                             <label for="expertise-level" style={{float:'left'}}>Expertise Level:</label>
-                            <input type = "text" className = "form-control" value={this.state.expertise} onChange={e => this.setState({expertise: e.target.value})}></input>
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.expertise} onChange={e => this.setState({expertise: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block"style={{marginBottom:'1em'}}onClick={this.newExpertise}>Save</button>
                             <label for="length" style={{float:'left'}}>Length:</label>
-                            <input type = "text" className = "form-control" value={this.state.length} onChange={e => this.setState({length: e.target.value})}></input>
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.length} onChange={e => this.setState({length: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block"style={{marginBottom:'1em'}}onClick={this.newLength}>Save</button>
                             <label for="intensity" style={{float:'left'}}>Intensity:</label>
-                            <input type = "text" className = "form-control" value={this.state.length} onChange={e => this.setState({length: e.target.value})}></input>
-
+                            <input type = "text" className = "form-control" placeholder={this.state.wrkt.intensity} onChange={e => this.setState({intensity: e.target.value})}></input>
+                            <button className="btn btn-primary btn-block"style={{marginBottom:'1em'}}onClick={this.newIntensity}>Save</button>
                         </Row>
                     </Container>
                 </Col>                                {/* Right Side Close */}
@@ -121,7 +156,7 @@ export class WorkoutPageEdit extends React.Component{
                 {this.state.wrkt.exercises.map((ex, i) => 
                 <Row>
                     <Col md={2}>
-                        <button className="btn btn-danger" style={{heigh:'4em', marginTop:'4em'}} onClick={event => this.removeExercise(i)}><i className="fa fa-trash"></i>Remove this workout</button>
+                        <button className="btn btn-danger btn-sm" style={{height:'4em', width:'12em', marginTop:'4em', left:'1em'}} onClick={event => this.removeExercise(i)}><i className="fa fa-trash"></i>Remove <br></br>this workout</button>
                     </Col>
                     <Col md={10}>
                         <ExerciseCard name={ex.name} desc={ex.desc} imageUrl={ex.imageUrl} length={ex.length} sets={ex.sets} reps={ex.reps}/>
@@ -192,7 +227,7 @@ export class WorkoutPageEdit extends React.Component{
               </Form>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={event => this.setState({ showAddExercise: false })}>
+              <Button variant="secondary"style={this.state.disp} onClick={event => this.setState({ showAddExercise: false })}>
                 Cancel
               </Button>
               <Button variant="primary" onClick={this.handleCustomAdditionSubmit}>
@@ -202,23 +237,40 @@ export class WorkoutPageEdit extends React.Component{
           </Modal>
 
 
-        </>);
+        </body>);
     }
 
+
+
+    
+
+
     componentDidMount() {
-        this.setState({
-            name: this.state.wrkt.name,
-            id: this.state.wrkt.id,
-            focus: this.state.wrkt.focus,
-            description: this.state.wrkt.description,
-            focus: this.state.wrkt.focus,
-            expertise: this.state.wrkt.expertise,
-            length: this.state.wrkt.length,
-            intensity: this.state.wrkt.intensity,
-            exercises: this.state.wrkt.exercises,
-            rating: this.state.wrkt.rating,
-            comments: this.state.wrkt.comments,
-        })
+      let workoutId = 102;
+        if (workoutId) {
+            this.workoutRepository.getWorkout(workoutId)
+                .then(wrkt => {
+                    var temp=[]
+                    
+                    for(let i=0;i<wrkt.length;i++){
+                      temp.push(new Exercise(wrkt[i].exercise_name, wrkt[i].exercise_desc, 
+                        wrkt[i].exercise_image, wrkt[i].default_length, wrkt[i].set_count, wrkt[i].rep_count))
+
+                    }
+                    var tempwork=new Workout(wrkt[0].workout_name, wrkt[0].workout_desc, 
+                        wrkt[0].category, wrkt[0].ExpLevel, wrkt[0].workout_length, wrkt[0].intensity,
+                        temp, wrkt[0].rating, wrkt[0].comments
+                        )
+                    this.setState({ wrkt: tempwork,
+                                    name: tempwork.name,
+                                    description: tempwork.description,
+                                    focus: tempwork.focus,
+                                    intensity: tempwork.intensity,
+                                    lenght: tempwork.length,
+                                    expertise: tempwork.expertise
+                                    })
+                  });
+        }
       }
 }
 
