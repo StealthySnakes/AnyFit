e7b6a63e77bf866aec16cfa51a0f8364f04114de
Chase Holzer <32146471+ckholzer27@users.noreply.github.com>
2019-05-02 03:07:34 -0500

    hopefully works

--- Frontend/src/api/SignupRepository.js
+++ Frontend/src/api/SignupRepository.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-export class AccountRepository {
-    url = "http://18.218.117.25:3000/";
+export class SignupRepository {
+    url = "http://18.218.117.25:3000";
     config = {
         headers: {
             Authorization: 'cholzer'
         }
     };
 
-    addAccount(account) {
+    addAccount(acc) {
+        
         return new Promise((resolve, reject) => {
-            axios.post(this.url/signup, account, this.config)
+            axios.post(`${this.url}/newuser`, acc, this.config)
                 .then(resp => resolve(resp.data))
                 .catch(resp => alert(resp));
         });
